0a120605da949464f64dbdfbbf43f9cdb71baf7f
Ariel Lubonja <ariellubonja@live.com>
2022-12-11 16:41:37 -0500

    Fix loadtxt to load()

--- Main/Python/tests/Z_check_correctness.py
+++ Main/Python/tests/Z_check_correctness.py
@@ -8,7 +8,7 @@ class TestStringMethods(unittest.TestCase):
     def test_Twitch(self):
         files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Twitch/"
-        Z_Correct = np.loadtxt(files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
         Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
@@ -18,7 +18,7 @@ class TestStringMethods(unittest.TestCase):
     def test_Pokec(self):
         files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Pokec/"
-        Z_Correct = np.loadtxt(files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
         Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
@@ -28,7 +28,7 @@ class TestStringMethods(unittest.TestCase):
     def test_LiveJournal(self):
         files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "LiveJournal/"
-        Z_Correct = np.loadtxt(files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
         Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
@@ -38,7 +38,7 @@ class TestStringMethods(unittest.TestCase):
     def test_Orkut(self):
         files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Orkut/"
-        Z_Correct = np.loadtxt(files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
         Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
@@ -49,7 +49,7 @@ class TestStringMethods(unittest.TestCase):
     def test_Orkut_Groups(self):
         files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Orkut-groups/"
-        Z_Correct = np.loadtxt(files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
         Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
