692679abf6185ad745d29731d83aab07ebebff90
Ariel Lubonja <ariel@cs.jhu.edu>
2022-12-11 21:41:50 -0500

    Add tests for Laplacian

--- Main/Python/tests/Z_check_correctness.py
+++ Main/Python/tests/Z_check_correctness.py
@@ -3,43 +3,40 @@ import unittest
 import numpy as np
 
 
-class TestStringMethods(unittest.TestCase):
+class TestAdjacency(unittest.TestCase):
+    files_dir = "../../../Experiments/Compare-Results/Adjacency/"
 
     def test_Twitch(self):
-        files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Twitch/"
-        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
-        Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06) # Atol for ligra - csv loses precision
 
         self.assertTrue(np.all(comparison))
 
     def test_Pokec(self):
-        files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Pokec/"
-        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
-        Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06) # Abs tolerance for ligra - csv loses precision
 
         self.assertTrue(np.all(comparison))
 
     def test_LiveJournal(self):
-        files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "LiveJournal/"
-        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
-        Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06)
 
         self.assertTrue(np.all(comparison))
 
     def test_Orkut(self):
-        files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Orkut/"
-        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
-        Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
 
@@ -47,15 +44,62 @@ class TestStringMethods(unittest.TestCase):
 
 
     def test_Orkut_Groups(self):
-        files_dir = "../../../Experiments/Compare-Results/"
         graph_path = "Orkut-groups/"
-        Z_Correct = np.load(files_dir + graph_path + "Z_CorrectResults.npy")
-        Z_to_check = np.loadtxt(files_dir + graph_path + "Z_to_check.csv")
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
 
         comparison = np.isclose(Z_Correct, Z_to_check)
 
         self.assertTrue(np.all(comparison))
 
+
+class TestLaplacian(unittest.TestCase):
+    files_dir = "../../../Experiments/Compare-Results/Laplacian/"
+
+    def test_Twitch(self):
+        graph_path = "Twitch/"
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
+
+        comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06)  # Atol for ligra - csv loses precision
+
+        self.assertTrue(np.all(comparison))
+
+    def test_Pokec(self):
+        graph_path = "Pokec/"
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
+
+        comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06)  # Abs tolerance for ligra - csv loses precision
+
+        self.assertTrue(np.all(comparison))
+
+    def test_LiveJournal(self):
+        graph_path = "LiveJournal/"
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
+
+        comparison = np.isclose(Z_Correct, Z_to_check, atol=1e-06)
+
+        self.assertTrue(np.all(comparison))
+
+    def test_Orkut(self):
+        graph_path = "Orkut/"
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
+
+        comparison = np.isclose(Z_Correct, Z_to_check)
+
+        self.assertTrue(np.all(comparison))
+
+    def test_Orkut_Groups(self):
+        graph_path = "Orkut-groups/"
+        Z_Correct = np.load(self.files_dir + graph_path + "Z_CorrectResults.npy")
+        Z_to_check = np.loadtxt(self.files_dir + graph_path + "Z_to_check.csv")
+
+        comparison = np.isclose(Z_Correct, Z_to_check)
+
+        self.assertTrue(np.all(comparison))
 
 if __name__ == '__main__':
     unittest.main()
