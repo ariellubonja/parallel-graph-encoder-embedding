771599660be83926adaf122c3b589e3bb3547248
Ariel Lubonja <ariel@cs.jhu.edu>
2023-06-08 19:13:15 -0400

    Add files via upload

--- Main/Python/Evaluation.py
+++ Main/Python/Evaluation.py
@@ -64,151 +64,48 @@ class Encoder_case:
 
 
 
-# def generateYlabelsSBM():
+def loadGraph(filepath, weighted, randomY=True, yPath=None):
+    print("Loading " + filepath)
+    
+    G_edgelist = np.loadtxt(filepath, delimiter=",")
+    
+    if not weighted:
+	# Add column of ones - weights
+    	G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
+    
+    n = int(np.max(G_edgelist[:,1]) + 1) # Nr. vertices
+    
+    if randomY: # Efficient Vector operations
+        n_nodes = int(max(np.max(G_edgelist[:,1]), np.max(G_edgelist[:,0])))
+    	# create some ground truth
+        labels = np.random.randint(low=0, high=50, size=(n_nodes,1))
+    	# Remove 90% of them by assigning -1
+        bern = np.random.binomial(1, 0.1, size=(n_nodes,1))
+        Y = labels * bern
+        # Subtract 1
+        Y -= 1
+    
+        # Make sure to assign 1 value to be maxvalue i.e. 49 (50-1) in Y - GEE uses max(Y) to set embedding size!
+        Y[0]=49
+    else:
+        Y = np.load(yPath)
+    
+    return G_edgelist, Y, n
+
     
 
 
 if __name__ == '__main__':
     
-    # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
-    import argparse
-
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-fn", "--filename", help="Input File name. .npy Edgelist required")
-    
-    args = argParser.parse_args()
-
-    print("args.name=%s" % args.filename)
-
-
-    if args.filename is not None:
-        print("Loading user-defined graph (unsupervised)")
-
-        G_edgelist = np.load(args.filename)
-
-        # Add column of ones - weights
-        G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1)))).astype(np.int32)
-
-        n = int(np.max([np.max(G_edgelist[:,0]), np.max(G_edgelist[:,1])])) # Nr. vertices
-
-        Y = np.full(shape=(n,1), fill_value=-1, dtype=np.int32)
-    else:
-        # A = np.ones((5,5))
-        # A[0,4] = 0
-        # A[4,0] = 0
-        # np.fill_diagonal(A, 0)
-        #
-        # Y = np.array([[0,0,0,1,1]]).reshape((5,1))
-        #
-        # # print(A)
-        # # print(Y)
-        #
-        # Encoder_case5 = Encoder_case(A,Y,5)
-        #
-        # from Main.Python.DataPreprocess import DataPreprocess
-        #
-        # Dataset = DataPreprocess(Encoder_case5, Laplacian = False, DiagA = False)
-        # # print(Dataset.X)
-        # # print(Dataset.Y)
-        # # print(Dataset.n)
-
-        # case = Case(n)
-        # case_10 = case.case_10() # This is O(n^2)
-        # case_10.summary()
-
-        #
-        # print("Running graph_encoder_embed()")
-        #
-        # Z, W = graph_encoder_embed(Dataset.X[0], Dataset.Y, Dataset.n, Correlation = False)
-        # print("Z:\n", Z)
-        # # print(W)
-
-
-        # print("Loading LiveJournal graph - 1GB")
-        #
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/liveJournalWgh.npy")
-        #
-        # # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/liveJournal.npy")
-        # # Add column of ones - weights
-        # # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
-        #
-        # n = int(np.max(G_edgelist[:,1]) + 1) # Nr. vertices
-        #
-        # Y = np.load("../../../Thesis-Graph-Data/Ys/liveJournal-Y50.npy")
-
-        # print("Loading Orkut graph - 1.8GB")
-        #
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/orkutWgh.npy")
-        #
-        # # Add column of ones - weights
-        # # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
-        #
-        # n = int(np.max(G_edgelist[:, 1]) + 1)  # Nr. vertices
-        #
-        # Y = np.load("../../../Thesis-Graph-Data/Ys/orkut-Y50.npy")
-
-
-        print("Loading Orkut-User2Group graph - 5.1GB")
-
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/orkut-groupsUnw.npy")
-        # # Add column of ones - weights
-        # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
-
-        G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/orkut-groupsWgh.npy")
-
-
-        n = int(np.max(G_edgelist[:, 1])) + 1  # Nr. vertices +1 bcs we index G[v] - 0-indexed
-
-        Y = np.load("../../../Thesis-Graph-Data/Ys/orkut-groups-Y40.npy")
-
-        # print("Loading Twitch graph")
-        #
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/twitchWgh.npy")
-        # # G_edgelist = np.loadtxt("../../../Thesis-Graph-Data/twitch-SNAP-bidir-manually", delimiter=" ", dtype=np.int32)
-        #
-        # G_edgelist = G_edgelist[G_edgelist[:, 0].argsort()] # Sort by first column
-        #
-        # # Add column of ones - weights
-        # # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))#.astype(np.int32)
-        # # Make sure G_edgelist isn't restricted to int-s
-        #
-        # n = int(np.max(G_edgelist[:,1]) + 1) # Nr. vertices
-        #
-        # Y = np.load("../../../Thesis-Graph-Data/Ys/twitch-Y20.npy")
-
-
-        # print("Loading Pokec graph - 400MB")
-        #
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/pokecWgh.npy")
-        #
-        # # Add column of ones - weights
-        # # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
-        #
-        # n = int(np.max(G_edgelist[:,1]) + 1) # Nr. vertices
-        #
-        # # Load Y from file
-        # Y = np.load("../../../Thesis-Graph-Data/Ys/pokec-Y50.npy")
-
-
-        # print("Loading Friendster graph - 31GB")
-        #
-        # G_edgelist = np.load("../../../Thesis-Graph-Data/NPY-graphs/friendsterWgh.npy")
-        #
-        # # Add column of ones - weights
-        # # G_edgelist = np.hstack((G_edgelist, np.ones((G_edgelist.shape[0], 1))))
-        #
-        # n = int(np.max(G_edgelist[:,1]) + 1) # Nr. vertices
-        #
-        # # Load Y from file
-        # Y = np.load("../../../Thesis-Graph-Data/Ys/friendster-Y50.npy")
+    G_edgelist, Y, n = loadGraph("/home/ariel/prog/leiden-runtime-experiments/carey-tsg-graphs/edgelist-msr-n32277-T24-forRB2.csv", weighted=True, randomY=True)
 
 
     laplacian=False
     print("Running GraphEncoderEmbed( laplacian =", laplacian, ")")
 
-    Z, _ = graph_encoder_embed(G_edgelist.astype(np.float64), Y, n, Correlation = False, Laplacian = laplacian)
+    Z, W = graph_encoder_embed(G_edgelist, Y, n, Correlation = False, Laplacian = laplacian)
 
     print("Saving Embedding to file")
-    # np.savetxt("Z_CorrectResults.csv", Z, fmt="%f")
-    np.save("EmbeddingResults.npy", Z)
+    np.savetxt("Z_CorrectResults.csv", Z, fmt="%f")
+    # np.save("Z_CorrectResults.npy", Z)
 
